73319beac95920766490482c50de6fdf6728f12b
Mohamed Abdalla <118936824+3bdallaaa@users.noreply.github.com>
2025-07-20 03:04:11 +0000

    Done with all

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -12,8 +12,27 @@ public class Ticket {
 
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+  public void setPrice(double price){
+    this.price = price;
+  }
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
 
   // Add a separate method to get the value of each field, called getDestination,
+  public String getDestination(){
+    return destination;
+  }
+
   // getPrice and getIsReturn.
+  public double getPrice(){
+    return price;
+  }
+  public boolean getIsReturn(){
+    return isReturn;
+  }
 
 }
--- _03_02/GradingSystem.java
+++ _03_02/GradingSystem.java
@@ -5,6 +5,8 @@ public class GradingSystem {
   public boolean isAPass(int percentage) {
     // Return true if the percentage is higher than or equal to 60.
     // Otherwise return false.
+    if(percentage>=60)
+      return true;
     return false;
   }
 
@@ -14,17 +16,33 @@ public class GradingSystem {
     // If it's 70-79, return 'C'.
     // If it's 60-69, return 'D'.
     // If it's less than 60, return 'F'.
-    return 'X';
+    
+    if (percentage>=90)
+      return 'A';
+    else if(percentage>=80 && percentage <90)
+      return 'B';
+    else if (percentage >= 70 && percentage < 80)
+      return 'C';
+    else if (percentage >= 60 && percentage < 70)
+      return 'D';
+    else 
+      return 'F';
   }
 
   public String retakeMessage(int percentage, boolean allowedToRetake) {
     // If percentage is less than 60 and allowedToRetake is true, return a String
     // that says "The student has been entered for a retake."
+    if(percentage < 60 && allowedToRetake==true)
+      return "The student has been entered for a retake.";
     // If percentage is less than 60 and allowedToRetake is false, return a String
     // that says "The student is not allowed to retake this exam."
+    else if (percentage < 60 && allowedToRetake == false)
+      return "The student is not allowed to retake this exam.";
     // If percentage is 60 or higher, return a String that says "A retake is not
     // required."
-    return "";
-  }
+    else 
+     return "A retake is not required.";
+
+  } 
 
 }
